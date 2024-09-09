9e86c0d00b7284551f5d025a89fb996a531c6e1a
Manuel <me.lm2207@gmail.com>
2024-09-09 12:15:21 -0500

    feat: create Calculator and Display components

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,9 @@
+import Display from "./Display";
+
+export default function Calculator() {
+  return (
+    <div className="bg-black border-gray-700 rounded-xl border-8">
+      <Display />
+    </div>
+  );
+}
---
+++ src/components/Display.jsx
@@ -0,0 +1,5 @@
+export default function Display() {
+  return (
+    <div className="flex justify-end items-center bg-gray-400 text-6xl" style={{ textShadow: "1.3px 1.3px #383838" }}>0</div>
+  );
+}
