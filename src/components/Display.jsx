e4a4058f39ae554dc466755f027e4e5b2040c4e1
Manuel <me.lm2207@gmail.com>
2024-09-11 13:34:16 -0500

    feat: add display value as a prop

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,10 @@
-export default function Display() {
+export default function Display({ value }) {
   return (
-    <div className="flex justify-end items-center bg-gray-400 text-6xl" style={{ textShadow: "1.3px 1.3px #383838" }}>0</div>
+    <div
+      className="flex justify-end items-center bg-gray-400 text-6xl m-2 p-2"
+      style={{ textShadow: "1.3px 1.3px #383838" }}
+    >
+      {value}
+    </div>
   );
 }
