e7ed244571ccc6482b9ebaa4c76b6b4bb9ec9b35
Manuel <me.lm2207@gmail.com>
2024-09-16 12:51:36 -0500

    feat: add sendInput function in button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,27 @@
+import { useContext } from "react";
+import { HandleInputContext } from "../contexts/HandleInputContext";
+
 export default function Button({ children, type, className = "" }) {
+  const handleInput = useContext(HandleInputContext);
   const colors = {
     number: "#0f766e",
     operator: "#fb923c",
     system: "#3b82f6",
   }
-  const newClassName = className + " flex justify-center items-center px-10 py-5 m-0.5 rounded-lg";
+  const newClassName = className + " flex justify-center items-center px-10 py-5 m-0.5 rounded-lg cursor-pointer";
+
+  function sendInput() {
+    const input = {
+      type,
+      value: children,
+    }
+    handleInput(input);
+  }
 
   return (
     <div className={newClassName}
       style={{ backgroundColor: colors[type] }}
+      onClick={sendInput}
     >
       {children}
     </div>
